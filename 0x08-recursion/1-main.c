1337e97972c2fdaad6c24f09d3b1bc962be0c1dd
Gzaros <templatewebvo@gmail.com>
2023-06-07 17:14:57 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * main - Salah eddine hamidi Gzaros
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+	_print_rev_recursion("\nColton Walker");
+	return (0);
+	
+}
