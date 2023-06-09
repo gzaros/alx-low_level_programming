d7ea0d4b8a8c077880fe902be0b0860a838b5cd8
Gzaros <templatewebvo@gmail.com>
2023-06-09 03:40:47 -0700

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -> Salah eddine hamidi Gzaros
+ * main -> Program
  *
  * @argc: Size array argv
  * @argv: Array of size argc
@@ -9,12 +9,9 @@
  * Return: 0 Always
  */
 
-
 int main(int argc __attribute__((unused)), char **argv)
 {
-
 	argc = 0;
 	printf("%s\n", argv[argc]);
 	return (0);
-	
-}
+}}
