39cc1e5635e76128a02658bdd9bfb49c946a93fd
gzaros <templatewebvo@gmail.com>
2023-07-01 00:14:44 +0100

    0x12-singly_linked_lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - Prints list_t elements.
+ * @h: SLL 
+ * Return: List size.
+ */
+ 
+size_t print_list(const list_t *h)
+{
+size_t nelem;
+nelem = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[%d] %s\n", 0, "(nil)");
+else
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+nelem++;
+}
+return (nelem);
+}
